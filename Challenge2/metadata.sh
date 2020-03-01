809cd62d80928df9ec8cebc6f908889ba52087e8
adityabalhara <aditya.balhara@gmail.com>
2020-03-01 18:44:48 +0000

    Updated with Echo command

--- Challenge2/metadata.sh
+++ Challenge2/metadata.sh
@@ -2,3 +2,4 @@
 yum install jq
 chmod u+x ec2-metadata
 ./ec2-metadata | jq --raw-input . | jq --slurp . | sed 's/:/": "/g' | sed 's/" /"/g' | sed 's/\[/\{/g' | sed 's/\]/\}/g' > metadata.json
+echo "Output stored in metadata.json"
