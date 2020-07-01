6a433f216fa973ab80da953acabada01c3cdb18d
adityabalhara <aditya.balhara@gmail.com>
2020-07-01 16:20:08 +0100

    Update metadata.sh

--- Challenge2/metadata.sh
+++ Challenge2/metadata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install jq
+yum install -y jq
 chmod u+x ./ec2-metadata.sh
 ./ec2-metadata.sh | jq --raw-input . | jq --slurp . | sed 's/:/": "/g' | sed 's/" /"/g' | sed 's/\[/\{/g' | sed 's/\]/\}/g' > metadata.json
 echo "Output stored in metadata.json"
